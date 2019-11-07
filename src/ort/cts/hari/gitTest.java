c5925d7d300c124beb93879e17916ae86797e7e9
karthikeyankandhan <karthikeyankandhan@gmail.com>
2019-11-07 12:26:25 +0530

    yjkl

--- src/ort/cts/hari/gitTest.java
+++ src/ort/cts/hari/gitTest.java
@@ -3,6 +3,12 @@ package ort.cts.hari;
 public class gitTest {
 
 	public static void main(String[] args) {
+		
+		System.out.println("id");
+		System.out.println("id");System.out.println("id");
+		System.out.println("id");
+		System.out.println("id");
+		
 		int id = 10;
 		int idh = 11;
 		int idl = 102;
