3a633a4ef9bb2a443e278b8c69df739c648c4359
karthikeyankandhan <karthikeyankandhan@gmail.com>
2019-11-07 12:21:54 +0530

    dghj

--- src/ort/cts/hari/gitTest.java
+++ src/ort/cts/hari/gitTest.java
@@ -3,9 +3,9 @@ package ort.cts.hari;
 public class gitTest {
 
 	public static void main(String[] args) {
-		int id = 10;
-		int idh = 11;
-		int idl = 102;
-		int ihjd = 102;
+		int id = 10252;
+		int idh = 1152;
+		int idl = 10252;
+		int ihjd = 10252;
 	}
 }
